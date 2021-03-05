124b02878c816341ec48132dd0e3edf7f9314c33
JF <jeanfrancois.marie@viacesi.fr>
2021-03-05 21:29:45 +0100

    Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="nav-logo">
+      <h1>CesInternship</h1>
+    </div>
+    <div class="nav-label">
+      <router-link to="/">Acceuil</router-link>
+      <router-link to="/offers">Offres</router-link>
+      <router-link to="/contact">Contacts</router-link>
+    </div>
+    <div class="nav-button">
+      <button>Connexion</button>
+    </div>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <style lang="scss">
-
-  @import 'assets/style/app.scss';
-  @import 'assets/style/navbar.scss';
-
+@import "assets/style/app.scss";
+@import "assets/style/navbar.scss";
 </style>
