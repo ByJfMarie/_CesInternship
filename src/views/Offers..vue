9600d3e3f765db918e061dd3f2e3977d10215574
Héléna <helena.oursel@gmail.com>
2021-03-08 13:35:27 +0100

    Offers debut

---
+++ src/views/Offers..vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-md-6">
+      <div row>
+          <fieldset>
+              <label>Recherche</label>
+              <input name="Recherche">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
