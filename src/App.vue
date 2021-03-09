8af835a8edc6eea51b4fc0a172cc878d3a8041a2
Jean-françois Marie <jfdu50@outlook.fr>
2021-03-09 10:51:12 +0100

    Responsive + Repiar Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <div id="nav">
+  <div class="nav" id="nav">
     <div class="nav-label" id="nav-label">
       <h1 class="nav-logo">CesInternship</h1>
-      <router-link to="/" class="nav-a">Accueil</router-link>
-      <router-link to="/offers" class="nav-a">Offres</router-link>
-      <router-link to="/contact" class="nav-a">Contacts</router-link>
-      <a href="#" class="icon" v-on:click="menu">
-        <i class="fa fa-bars"></i>
-      </a>
+      <router-link to="/">Accueil</router-link>
+      <router-link to="/offers">Offres</router-link>
+      <router-link to="/contact">Contacts</router-link>
+      <button @click="responsive" href="#" class="icon">
+        <i class="fa fa-bars fa-2x"></i>
+      </button>
     </div>
     <div class="nav-button">
-      <button>Connexion</button>
+      <button >Connexion</button>
     </div>
   </div>
   <router-view />
@@ -25,12 +25,14 @@
 <script>
 export default {
   methods: {
-
-    menu: function() {
-
-      var navbar = document.getElementsByClassName("nav-a")
-      for (let i = 0; i < navbar.length; i++) {
-        navbar[i].style.display = "inline-block";
+    responsive: function () {
+      console.log("Ouverture");
+      let nav = document.getElementById("nav");
+      if (nav.className == "nav") {
+        nav.className += "responsive";
+      }else{
+        nav.className = "nav";
+        
       }
     }
   }
