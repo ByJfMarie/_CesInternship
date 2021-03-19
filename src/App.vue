44da8555707aa0210c3eeb523ec7b084abba5742
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2021-03-19 15:13:50 +0100

    nav fixed (vraiment cette fois)

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,17 @@
 <template>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <div class="nav" id="nav">
-    <div class="nav-label" id="nav-label">
-      <h1 class="nav-logo">CesInternship</h1>
-      <router-link to="/">Accueil</router-link>
-      <router-link to="/offers">Offres</router-link>
-      <router-link to="/contact">Contacts</router-link>
-      <button @click="responsive" href="#" class="icon">
-        <i class="fa fa-bars fa-2x"></i>
-      </button>
-    </div>
-  </div>
   <router-view />
 </template>
 
 <style lang="scss">
 @import "assets/style/app.scss";
-@import "assets/style/navbar.scss";
+@import 'assets/style/navbar.scss';
 </style>
 
 
 <script>
 
-
 export default {
-  methods: {
-    responsive: function () {
-      console.log("Ouverture");
-      let nav = document.getElementById("nav");
-      if (nav.className == "nav") {
-        nav.className += "responsive";
-      }else{
-        nav.className = "nav";
-        
-      }
-    }
-  }
+
 }
 </script>
