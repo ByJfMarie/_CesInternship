74635eae33ef1819829f61b85412b73bc7f23821
Baptiste Burguiere <baptiste.burguiere@viacesi.fr>
2021-03-22 11:12:46 +0100

    azd

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,62 +1,20 @@
 <template>
-  <div class="connexion">
-<<<<<<< HEAD
-      <h1>Connexion</h1>
-      <div class="form-container">
-          <form action="" method="post" class="form-connection">
-              <h2>You already have a account ?</h2>
-=======
-      <h1>Connection</h1>
-          <form action="" method="post">
->>>>>>> a2106c812b6f11c3dd48e29fd317a734e83064ac
-              <p>
-                  Email <br>
-                  <input type="text" id="email" name="email">
-              </p>
-              <p>
-                  Password <br>
-                  <input type="text" id="pswd" name="pswd">
-              </p>
-              
-              <input type="submit" value="Submit" class="btn-valider">
-              <input type="reset" value="Reset" class="btn-reset">
-          </form>
-<<<<<<< HEAD
-          <form action="" method="post" class="form-create">
-              <h2>Don't have a account yet ?</h2>
-              <p>
-                  First Name<br>
-                  <input type="text" id="nom" name="nom">
-              </p>
-              <p>
-                  Last Name <br>
-                  <input type="text" id="prenom" name="prenom">
-              </p>
-              <p>
-                  Email <br>
-                  <input type="text" id="email" name="email">
-              </p>
-              <p>
-                  Center <br>
-                  <input type="text" id="centre" name="centre">
-              </p>
-              <p>
-                  Promotion <br>
-                  <input type="text" id="promo" name="promo">
-              </p>
-              <p>
-                  Password <br>
-                  <input type="text" id="pswd" name="pswd">
-              </p>
-        
-              <input type="submit" value="Submit" class="btn-valider">
-              <input type="reset" value="Reset" class="btn-reset">
+    <div class="connexion">
+        <h1>Connection</h1>
+        <form action="" method="post">
+            <p>
+                Email <br>
+                <input type="text" id="email" name="email">
+            </p>
+            <p>
+                Password <br>
+                <input type="text" id="pswd" name="pswd">
+            </p>
 
-          </form>
-      </div>
-=======
->>>>>>> a2106c812b6f11c3dd48e29fd317a734e83064ac
-  </div>
+            <input type="submit" value="Submit" class="btn-valider">
+            <input type="reset" value="Reset" class="btn-reset">
+        </form>
+    </div>
 </template>
 
 <script>
---
+++ src/components/OfferCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="card">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OfferCard'
+}
+</script>
+
+<style scoped lang='scss'>
+    @import '../assets/style/offerCard.scss';
+</style>
