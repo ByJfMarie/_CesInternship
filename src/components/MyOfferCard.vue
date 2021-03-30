cf649ec4c5c66dd9953b2d40b84ae492bc2ec5af
Héléna <helena.oursel@gmail.com>
2021-03-30 19:18:25 +0200

    Steps

--- src/components/MyOfferCard.vue
+++ src/components/MyOfferCard.vue
@@ -1,22 +1,31 @@
 <template>
-
-    <div class="myoffer-container">
-        <span><a href="">Offer's Name - Company</a></span>
-        <div class="tags-container">
-            <span class="statut">Statut</span>
-            <input class="delete" type="button" value="Delete">
-        </div>
-        
+  <div class="myoffer-container">
+    <span><a href="">Offer's Name - Company</a></span>
+    <div class="tags-container">
+         <input class="delete" type="button" value="Delete" />
+      <div class="container">
+  <ul class="progressbar">
+    <li class="active">Apply to offer</li>
+    <li>Answer of the company</li>
+    <li>Validation send</li>
+    <li>Validation signed</li>
+    <li>Convention send</li>
+    <li>Convention signed</li>
+  </ul>
+      </div>
     </div>
-
+  </div>
 </template>
 
 <script>
 export default {
-    name: "MyOfferCard"
-}
+  name: "MyOfferCard",
+  methods:{
+      
+  }
+};
 </script>
 
 <style scoped lang="scss">
-  @import "../assets/style/myOfferCard.scss";
+@import "../assets/style/myOfferCard.scss";
 </style>
